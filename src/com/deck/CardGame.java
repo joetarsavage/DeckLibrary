088ddec9afd2499aa5c516bfc3aa7413bf8abcb1
Joe Tarsavage <jtarsava@its.jnj.com>
2018-07-27 14:22:37 -0400

    added utilities import, added more to game

--- src/com/deck/CardGame.java
+++ src/com/deck/CardGame.java
@@ -2,6 +2,7 @@ package com.deck;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import com.utilities.*;
 
 public class CardGame {
 	public static void main(String[] args) throws InterruptedException {
@@ -15,7 +16,8 @@ public class CardGame {
 		
 		System.out.println("Lets play");
 		
-		System.out.println("Press P then ENTER if the cards are a Pair");
+		System.out.println("Press Y then ENTER if the cards are a Pair");
+		System.out.println("Press N then ENTER if the cards are not a Pair");
 		
 		Thread.sleep(1000);
 		
@@ -25,9 +27,37 @@ public class CardGame {
 		
 		System.out.println("GO!");
 		
+		long startTime = System.nanoTime();
 		
-	
+		boolean result = true;
+		
+		for(int i = 0; i<26; i++) {
+			System.out.println(firstHalf.get(i) + " || " + secondHalf.get(i));
+			String input = scanner.nextLine().toLowerCase();
+			if ((Deck.isSameValue(firstHalf.get(i), secondHalf.get(i)) && input.equals("n")) 
+					|| (!(Deck.isSameValue(firstHalf.get(i), secondHalf.get(i))) && input.equals("y")) || !(input.equals("y") || input.equals("n"))) {
+				result = false;
+				break;
+			}
+		}
+		
+		long endTime = System.nanoTime();
 		
 		scanner.close();
+		
+		if(result == false) {
+			System.out.println("Sorry, that is incorrect. Better luck next time");
+			return;
+		}
+	
+		long duration = (endTime-startTime)/1000000;
+		
+		double finalTime = duration/1000.0000;
+		
+		System.out.println("Congratulations! Your time was " + finalTime + " seconds");
+		
+		System.out.println(Utilities.readFromFile("/Users/jtarsavage/score.txt"));
+		
+		
 	}
 }
